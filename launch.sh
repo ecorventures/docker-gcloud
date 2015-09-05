5281aaefc29122deb0d3ed4e3601eb1376d4aef8
Corey Butler <corey@coreybutler.com>
2015-09-05 11:20:25 -0500

    Added account specificity

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /key/credentials.json --project "$PROJECT" -q
+/google-cloud-sdk/bin/gcloud auth activate-service-account "$ACCOUNT" --key-file /key/credentials.json --project "$PROJECT" -q
 /google-cloud-sdk/bin/gcloud $*
